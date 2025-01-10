c1a20f7fe253b235670c56ed9b7f287ae6217705
dong <1749492810@qq.com>
2025-01-10 18:39:45 +0800

    SqlSugarRepository数据处理层实现

---
+++ dong.DLL/SqlSugar/ISqlSugarRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace dong.DLL
+{
+    public interface ISqlSugarRepository<T> : ISugarRepository, ISimpleClient<T> where T : class, new()
+    {
+       
+
+    }
+}
---
+++ dong.DLL/SqlSugar/SqlSugarRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace dong.DLL.SqlSugar
+{
+    internal class SqlSugarRepository<T> : SimpleClient<T>,ISqlSugarRepository<T> where T : class, new()
+    {
+    }
+}
